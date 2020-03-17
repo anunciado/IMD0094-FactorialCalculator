e6a5319be84f3714d99abed57b1aad2b069f5407
Luís Eduardo <luis.eduardo.225@gmail.com>
2020-03-17 08:32:35 -0300

    Adding comments and removing envs.

--- main.py
+++ main.py
@@ -3,25 +3,37 @@ from argparse import ArgumentParser
 from math import factorial
 
 def main():
-    ap = ArgumentParser()
+    # Setting pattern for redis
+    pattern = 'factorial:'
 
+    # Searching for parameters passed in the terminal
+    ap = ArgumentParser()
     ap.add_argument("-a", "--address", required=True, help="address for connection with redis")
     ap.add_argument("-p", "--port", required=True, help="port for connection with redis")
     args = vars(ap.parse_args())
 
+    # Instantiating an object that connects to a redis database using the parameters passed
     db = Database(host=args['address'], port=int(args['port']), db=0)
     print("--- Welcome to Factorial Calculator ---")
     while True:
         number = input("Please enter a non negative integer number: ")
-        if number.isalpha() == True or int(number) < 0:
-            print("You didn't write non negative integer!")
-        else:
-            if(db.get(number) is None):
-                print("--- Calculating factorial ---")
-                db[number] = factorial(int(number))
+
+        # Checking if the entered string is alphanumeric or, if it is a number, less than 0
+        try:
+            if number.isalpha() == True or int(number) < 0:
+                print("You didn't write non negative integer!")
             else:
-                print("--- Fetching result in cache ---")
-            print(db[number].decode())
+                # Checking if the number has already been calculated and is in the cache
+                if(db.get(pattern + number) is None):
+                    print("--- Calculating factorial ---")
+                    # Setting result in cache
+                    db[pattern + number] = factorial(int(number))
+                else:
+                    print("--- Fetching result in cache ---")
+                # Fetching result in cache
+                print(db[pattern + number].decode())
+        except ValueError:
+                print("You didn't write non negative integer!")
 
 
 if __name__ == '__main__':
